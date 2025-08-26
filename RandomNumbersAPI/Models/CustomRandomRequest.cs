14caed3379747e8f718c759ae0b350d41a890b0b
Ervin Yabeta Martinez <a2024111378@estudiantes.upsa.edu.bo>
2025-08-25 20:44:40 -0400

    AGREGAR: se agrego Random number, decimal, string y custom.

--- RandomNumbersAPI/Models/CustomRandomRequest.cs
+++ RandomNumbersAPI/Models/CustomRandomRequest.cs
@@ -1,6 +1,11 @@
-namespace RandomNumbersAPI.Models;
-
-public class CustomRandomRequest
+namespace RandomNumbersAPI.Models
 {
-    
+    public class CustomRandomRequest
+    {
+        public string Type { get; set; } = string.Empty; // "number", "decimal", "string"
+        public int? Min { get; set; }
+        public int? Max { get; set; }
+        public int? Decimals { get; set; } = 2;
+        public int? Length { get; set; } = 8;
+    }
 }
